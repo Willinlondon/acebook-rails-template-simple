0fbb93c701ded8dbe654622f9135a6da820c9220
Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>
2021-09-14 16:32:26 +0100

    generated comment scaffold

---
+++ db/migrate/20210914152715_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.integer :post_id
+      t.integer :user_id
+      t.string :body
+      t.belongs_to :post, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
