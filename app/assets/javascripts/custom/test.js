3ff6fb456d74e0726c26bc96e3c3116ffb87cbd3
ian k <kickthechair@gmail.com>
2021-09-21 19:10:02 +0100

    more JS prodding

--- app/assets/javascripts/custom/test.js
+++ app/assets/javascripts/custom/test.js
@@ -4,20 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('INSIDE');
 
   const allLikes = document.querySelectorAll(".like")
-
+  // let like = 0;
+  // document.getElementById('like_count').innerText = like
   allLikes.forEach(element => 
     //console.log(element)
     element.addEventListener('submit', () => {
+      let like = document.getElementById('like_count').innerText = 0
       event.preventDefault();
-      console.log('I\'ve been clicked')
-
-
-
+      console.log(typeof like)
+      document.getElementById('like_count').innerText = like++
     })
   );
   
-  
-  
-
 
 })
