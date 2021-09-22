f11a2955c71bf2173ef44665f0127a0f53ae4a0c
eballantine <lizzy.kayballantine@gmail.com>
2021-09-22 16:22:12 +0100

    WIP adding avatar and name to show alongside posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
   end
   
   private def post_params
-    params.require(:post).permit(:message, :avatar, :created_at)
+    params.require(:post).permit(:message, :avatar, :user_id, :created_at)
   end
 
   def find_post
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   has_one_attached :avatar
   has_many :likes
 
