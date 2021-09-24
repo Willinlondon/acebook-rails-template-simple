f115a459c01a8e2185d10feffc365fcabca9b02f
moby-codes <mubashar.jaffri@gmail.com>
2021-09-24 14:35:42 +0100

    terms page completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :posts, :users, :likes, :comments
   root 'users#index' #I think this needs to be under resources to work correctly!
-  get 'terms_path', to: 'users#terms'
-  #get 'terms', to: 'users#terms'
+  get 'terms', to: 'terms#index'
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create', as: 'log_in'
   delete 'logout', to: 'sessions#destroy'
