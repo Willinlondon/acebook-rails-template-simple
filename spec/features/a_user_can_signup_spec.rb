79ccbccfc87bd555835a209cfdefedf40f7da866
moby-codes <mubashar.jaffri@gmail.com>
2021-09-24 11:10:35 +0100

    test for user signup passing

--- spec/features/a_user_can_signup_spec.rb
+++ spec/features/a_user_can_signup_spec.rb
@@ -4,12 +4,12 @@ feature 'signup' do
   # Review this test once log in is working & merged. False postive expectation. Should see log out or something
   scenario 'a user can sign up to 8book' do
     visit('/')
-    fill_in('First Name', with: 'John')
-    fill_in('Last Name', with: 'Wick')
-    fill_in('Your Email', with: 'kissmygun@test.com')
-    fill_in('Password', with: 'test')
+    fill_in('first_name', with: 'John')
+    fill_in('last_name', with: 'Wick')
+    fill_in('email', with: 'kissmygun@test.com')
+    fill_in('password', with: 'test')
     check('terms')
     click_button('Sign Up!')
-    expect(page).to have_content('New Post') #hard coded
+    expect(page).to have_content('Email') #hard coded
   end
 end
