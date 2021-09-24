a21fc00047f0305fe11cbe0b9aa05e9e98f48be2
William Day <williamjday11@gmail.com>
2021-09-24 14:19:08 +0100

    Still attempting routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :posts, :users, :likes, :comments
   root 'users#index' #I think this needs to be under resources to work correctly!
-  get 'terms', to: 'users#terms'
+  get 'terms_path', to: 'users#terms'
+  #get 'terms', to: 'users#terms'
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create', as: 'log_in'
   delete 'logout', to: 'sessions#destroy'
